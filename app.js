63312e6ca4c3f01e610c36183ef28ad6e3494398
Zubairu Awudu <zubairuawudu123@gmail.com>
2025-01-12 15:05:19 -0500

    External API

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
 import { getMovies } from "./axios.js";
 
 getMovies();
+
+const movieGrid = document.getElementById('movie-grid');
+
+async function displayMovies(){
+  try {
+    const movies = await getMovies();
+    // console.log(movies.data.results);
+    const moviesArray = movies.data.results;
+    for(let i = 0; i < moviesArray.length; i++){
+      console.log(moviesArray[i]);
+      const movieCard = document.createElement('div');
+      movieCard.classList.add('movie-card');
+      movieCard.innerHTML = `
+        <img src="https://image.tmdb.org/t/p/w500/${moviesArray[i].poster_path}" alt="${moviesArray[i].title}">
+        <h2>${moviesArray[i].title}</h2>
+        <p>${moviesArray[i].overview}</p>
+      `;
+      movieGrid.appendChild(movieCard);
+      
+    }
+    
+
+  } catch (error) {
+    console.log(error);
+    
+  }
+
+
+  
+}
+displayMovies();
--- axios.js
+++ axios.js
@@ -16,14 +16,14 @@ import { getToken } from "./token.js";
 
 
     const api_key = '8a5b4e56bd1380f3a36a6c6dcb419bf7'
-    const request = await axios.get('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc',{
+     return await axios.get('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc',{
       headers: {
         Authorization: getToken()
       }
     })
-    const response = request.data
     
-    console.log(response);
+    
+    
     
   } catch (error) {
     console.log(error);
